51861f69c435f8f1b1e8bc712892b4a3295d3773
lewys-miugo <lewiswambugu01@gmail.com>
2025-06-29 10:43:39 +0300

    Writing fetch request to got api

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      renderBooks(data);
+    })
+    .catch(error => {
+      console.error('There has been a problem with your fetch operation:', error);
+    });
   
 }
 
